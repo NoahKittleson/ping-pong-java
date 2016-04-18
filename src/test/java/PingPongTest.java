4a93638c5dd135f1c45e3efcb70f8ac915ec5537
Ian Lesikar and Noahkittleson <noahkittleson@>
2016-04-18 14:03:08 -0700

    final version with working tests

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -6,12 +6,55 @@ import java.util.List;
 public class PingPongTest {
 
   @Test
-  public void isPingPong_forInputNumberDivisibleByThree_true() {
+  public void isPingPong_forInputNumber_ArrayList() {
     PingPong testPingPong = new PingPong();
     ArrayList<String> returnVals = new ArrayList<String>();
     returnVals.add("1");
     returnVals.add("2");
     assertEquals(returnVals, testPingPong.pingPong(2));
+  }
 
+  @Test
+  public void isPingPong_forInputNumberDivisibleByThree_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    ArrayList<String> returnVals = new ArrayList<String>();
+    returnVals.add("1");
+    returnVals.add("2");
+    returnVals.add("Ping");
+    assertEquals(returnVals, testPingPong.pingPong(3));
+  }
+
+  @Test
+  public void isPingPong_forInputNumberDivisibleByFive_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    ArrayList<String> returnVals = new ArrayList<String>();
+    returnVals.add("1");
+    returnVals.add("2");
+    returnVals.add("Ping");
+    returnVals.add("4");
+    returnVals.add("Pong");
+    assertEquals(returnVals, testPingPong.pingPong(5));
+  }
+
+  @Test
+  public void isPingPong_forInputNumberDivisibleByFifteen_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    ArrayList<String> returnVals = new ArrayList<String>();
+    returnVals.add("1");
+    returnVals.add("2");
+    returnVals.add("Ping");
+    returnVals.add("4");
+    returnVals.add("Pong");
+    returnVals.add("Ping");
+    returnVals.add("7");
+    returnVals.add("8");
+    returnVals.add("Ping");
+    returnVals.add("Pong");
+    returnVals.add("11");
+    returnVals.add("Ping");
+    returnVals.add("13");
+    returnVals.add("14");
+    returnVals.add("Ping Pong");
+    assertEquals(returnVals, testPingPong.pingPong(15));
   }
 }
