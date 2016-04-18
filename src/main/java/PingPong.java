10de90350e583bf3ec16127460e58413d53ac8a9
Ian Lesikar and Noahkittleson <ian@>
2016-04-18 11:58:58 -0700

    initial files

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,44 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import java.util.ArrayList;
+import java.util.List;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class PingPong {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, respone) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/output.vtl");
+
+      String countTo = request.queryParams("countTo");
+      Integer countToInteger = Integer.parseInt(countTo);
+      List<String> pingPongArray = pingPong(countToInteger);
+
+      model.put("countTo", countTo);
+      model.put("pingPongArray", pingPongArray);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+
+  public static List<String> pingPong(Integer inputNumber) {
+    List<String> returnVals = new ArrayList<String>();
+    for (Integer i = 0; i < inputNumber; i++) {
+      if (inputNumber % 3 == 0) {
+        returnVals.add("Ping");
+      } else {
+        returnVals.add(Integer.toString(inputNumber));
+      }
+    }
+    return returnVals;
+  }
+}
---
+++ src/test/java/PingPongTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PingPongTest {
+
+  @Test
+  public void isPingPong_forInputNumberDivisibleByThree_true() {
+    PingPong pingPong = new PingPong();
+  }
+}
